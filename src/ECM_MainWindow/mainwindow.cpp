8fd9a4c319fcfc74e24b512872902fb11e17a9ed
kkroeger007 <ken.kroeger@heronsystems.com>
2017-05-29 19:00:51 -0400

    Testing Code

--- src/ECM_MainWindow/mainwindow.cpp
+++ src/ECM_MainWindow/mainwindow.cpp
@@ -6,9 +6,79 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    int decimal = 4191;
+    QByteArray B2;
+    char test;
+    test = static_cast<char>(01);
+    B2.append(test);
+    Buffer[0] = test;
+
+    test = static_cast<char>(03);
+    B2.append(test);
+    Buffer[1] = test;
+
+//    char HIGH = (char)((decimal & 0xFF00) >> 8);
+//    char LOW = (char)(decimal & 0x00FF);
+//    B2.append(HIGH);
+//    B2.append(LOW);
+//    Buffer[2] = HIGH;
+//    Buffer[3] = LOW;
+
+    test = static_cast<char>(00); //00
+    B2.append(test);
+    Buffer[2] = test;
+
+    test = static_cast<char>(10);
+    B2.append(test);
+    Buffer[3] = test;
+
+    test = static_cast<char>(0);
+    B2.append(test);
+    Buffer[4] = test;
+
+    test = static_cast<char>(1);
+    B2.append(test);
+    Buffer[5] = test;
+
+    test = static_cast<char>(0);
+    B2.append(test);
+    Buffer[8] = test;
+
+    test = static_cast<char>(16);
+    B2.append(test);
+    Buffer[9] = test;
+
+    test = static_cast<char>(178);
+    B2.append(test);
+    Buffer[10] = test;
+
+    CRC16(6);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+WORD MainWindow::CRC16(const int &cnt)
+{
+    char c,j;
+    unsigned int Temp = 0xFFFF;
+
+    for (int i=0;i<cnt;i++){
+        Temp ^= (WORD)Buffer[i];
+        for (j=8;j!=0;j--){
+
+            if ((Temp & 0x0001) != 0) {      // If the LSB is set
+                Temp >>= 1;                    // Shift right and XOR 0xA001
+                Temp ^= 0xA001;
+            }
+            else                            // Else LSB is not set
+                Temp >>= 1;                    // Just shift right
+        }
+    }
+    char HIGH = (char)((Temp & 0xFF00) >> 8);
+    char LOW = (char)(Temp & 0x00FF);
+
+    return Temp;
 }
--- src/ECM_MainWindow/mainwindow.h
+++ src/ECM_MainWindow/mainwindow.h
@@ -1,7 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QByteArray>
+#include <QString>
+#include <QtDebug>
+#include <iostream>
 #include <QMainWindow>
+#include <vector>
+
+typedef unsigned int WORD;
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +21,11 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    WORD CRC16(const int &cnt);
 
 private:
     Ui::MainWindow *ui;
+    char Buffer[128];
 };
 
 #endif // MAINWINDOW_H
