b60b1bce07b8571c765cb252c9868becccc7865a
ken <ken.kroeger@heronsystems.com>
2018-02-04 09:02:07 -0500

    Small changes to check the initial info of the rigol.

---
+++ src/library_rigol_oscilloscope/rigol_poll_measurements.cpp
@@ -0,0 +1,68 @@
+#include "galil_poll_status.h"
+
+GalilPollState::GalilPollState(const int &msTimeout):
+    m_CB(nullptr), timeout(msTimeout)
+{
+
+}
+
+void GalilPollState::beginPolling()
+{
+    m_Timeout.start();
+    this->start();
+}
+
+void GalilPollState::pausePolling()
+{
+    m_LambdasToRun.push_back([this]{
+        m_Timeout.stop();
+        clearPendingTasks();
+        mToExit = true;
+    });
+}
+
+void GalilPollState::run()
+{
+    while(true)
+    {
+        if(mToExit == true) {
+            clearPendingTasks();
+            m_Timeout.stop();
+            break;
+        }
+
+        this->RunPendingTasks();
+
+        //The current state we can find out how much time has passed.
+        //If one of the lambda expressions has fired the clock should
+        //be reset right at the end, thus making this value small and
+        //improbable the next function will fire
+        double timeElapsed = m_Timeout.elapsedMilliseconds();
+
+        if(timeElapsed > timeout)
+        {
+            //this means we should request the state of all the standard IO/Errors/Pos of the galil unit
+            //these functions will update the actual stored information of the galil unit in the state
+            //interface class contained at the callback location
+
+            // 1: Request the position of the galil unit
+            if(m_CB)
+            {
+//                RequestTellPositionPtr requestTP = std::make_shared<RequestTellPosition>();
+//                m_CB->cbi_GalilStatusRequest(requestTP);
+                // 2: Request the stop codes
+//                RequestStopCodePtr requestSC = std::make_shared<RequestStopCode>();
+//                m_CB->cbi_GalilStatusRequest(requestSC);
+                // 3: Request the tell switches
+                RequestTellSwitchesPtr requestTS = std::make_shared<RequestTellSwitches>();;
+                m_CB->cbi_GalilStatusRequest(requestTS);
+                // 4: Request the current inputs
+                RequestTellInputsPtr requestTI = std::make_shared<RequestTellInputs>();
+                m_CB->cbi_GalilStatusRequest(requestTI);
+            }
+            m_Timeout.reset();
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(timeout));
+    }
+}
---
+++ src/library_rigol_oscilloscope/rigol_poll_measurements.h
@@ -0,0 +1,63 @@
+#ifndef GALIL_STATE_MACHINE_H
+#define GALIL_STATE_MACHINE_H
+
+#include <iostream>
+#include <QDate>
+
+#include "common/environment_time.h"
+#include "common/threadmanager.h"
+#include "common/timer.h"
+
+#include "requests/request_components.h"
+
+class GalilStatusUpdate_Interface
+{
+public:
+    virtual void cbi_GalilStatusRequest(const AbstractRequestPtr request) = 0;
+};
+
+class GalilPollState : public Thread
+{
+public:
+    GalilPollState(const int &msTimeout = 50);
+
+    ~GalilPollState() {
+        std::cout << "Destructor on the galil timeout state machine" << std::endl;
+        mToExit = true;
+    }
+
+    void beginPolling();
+    void pausePolling();
+
+    void run();
+
+    void connectCallback(GalilStatusUpdate_Interface *cb)
+    {
+        m_CB = cb;
+    }
+
+private:
+    Timer m_Timeout;
+    int timeout;
+
+private:
+    GalilStatusUpdate_Interface *m_CB;
+
+protected:
+    std::list<std::function<void()>> m_LambdasToRun;
+
+    void clearPendingTasks()
+    {
+        m_LambdasToRun.clear();
+    }
+
+    void RunPendingTasks() {
+        while(m_LambdasToRun.size() > 0) {
+            auto lambda = m_LambdasToRun.front();
+            m_LambdasToRun.pop_front();
+            lambda();
+        }
+    }
+};
+
+#endif // GALIL_STATE_MACHINE_H
