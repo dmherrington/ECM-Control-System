57cfcfc6fc704095dccb531c7873a284b3607446
majorpr13 <kkroeger@araustech.com>
2018-09-17 15:50:55 -0400

    Forgot Pump Global File

--- src/library_westinghouse510/library_westinghouse510_global.h
+++ src/library_westinghouse510/library_westinghouse510_global.h
@@ -1,16 +1,23 @@
 #ifndef LIBRARY_WESTINGHOUSE510_GLOBAL_H
 #define LIBRARY_WESTINGHOUSE510_GLOBAL_H
 
+#include <QtCore/qglobal.h>
 #include "westinghouse_version.h"
 
-#ifdef _MSC_VER
-#  if defined(LIBRARY_WESTINGHOUSE510_LIBRARY)
-#    define LIBRARY_WESTINGHOUSE510SHARED_EXPORT __declspec(dllexport)
-#  else
-#    define LIBRARY_WESTINGHOUSE510SHARED_EXPORT __declspec(dllexport)
-#  endif
+#if defined(LIBRARY_WESTINGHOUSE510_LIBRARY)
+#  define LIBRARY_WESTINGHOUSE510SHARED_EXPORT Q_DECL_EXPORT
 #else
-#  define LIBRARY_WESTINGHOUSE510SHARED_EXPORT
+#  define LIBRARY_WESTINGHOUSE510SHARED_EXPORT Q_DECL_IMPORT
 #endif
+
+//#ifdef _MSC_VER
+//#  if defined(LIBRARY_WESTINGHOUSE510_LIBRARY)
+//#    define LIBRARY_WESTINGHOUSE510SHARED_EXPORT __declspec(dllexport)
+//#  else
+//#    define LIBRARY_WESTINGHOUSE510SHARED_EXPORT __declspec(dllexport)
+//#  endif
+//#else
+//#  define LIBRARY_WESTINGHOUSE510SHARED_EXPORT
+//#endif
 
 #endif // LIBRARY_WESTINGHOUSE510_GLOBAL_H
